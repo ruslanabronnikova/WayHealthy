8939ff348460eb55824be44a4e236c89c92a49ef
Ruslana <bronnikovars@yandex.ru>
2024-02-08 14:51:36 +0300

    Верстка сайта Путь здоровья

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const sendButton = document.querySelector('.button'); // Выбираем кнопку "Отправить"
-    const modalSignin = document.getElementById('modalSignin');
+  const sendButton = document.querySelector('.button'); // Выбираем кнопку "Отправить"
+  const modalSignin = document.getElementById('modalSignin');
 
-    sendButton.addEventListener('click', function(event) {
-      event.preventDefault(); // Предотвращаем стандартное действие кнопки "Отправить"
+  sendButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное действие кнопки "Отправить"
 
-      // Открываем модальное окно
+    // Открываем модальное окно
+    var myModal = new bootstrap.Modal(modalSignin);
+    myModal.show();
+  });
+
+  // Находим крестик для закрытия модального окна
+  const closeButton = modalSignin.querySelector('.btn-close');
+  // Добавляем обработчик события клика на крестик
+  closeButton.addEventListener('click', function() {
+    var myModal = new bootstrap.Modal(modalSignin);
+    myModal.hide(); // Закрываем модальное окно при клике на крестик
+  });
+
+  // Добавляем обработчик события клика на задний фон
+  modalSignin.addEventListener('click', function(event) {
+    if (event.target === modalSignin) {
       var myModal = new bootstrap.Modal(modalSignin);
-      myModal.show();
-      
-      // Добавляем класс d-block для отображения модального окна
-      modalSignin.classList.add('d-block');
-    });
+      myModal.hide(); // Закрываем модальное окно при клике на задний фон
+    }
+  });
 });
