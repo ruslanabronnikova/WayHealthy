a13d9763a94f30828a14648928d44d9bb8917a88
Ruslana <bronnikovars@yandex.ru>
2024-02-08 17:50:44 +0300

    Правки по верстке

--- assets/js/accordeon.js
+++ assets/js/accordeon.js
@@ -1,28 +1,32 @@
 class ItcAccordion {
-    constructor(target, config) {
-      this._el = typeof target === 'string' ? document.querySelector(target) : target;
-      const defaultConfig = {
-        alwaysOpen: true
-      };
-      this._config = Object.assign(defaultConfig, config);
-      this.addEventListener();
-    }
-    addEventListener() {
-      this._el.addEventListener('click', (e) => {
-        const elHeader = e.target.closest('.accordion__header');
-        if (!elHeader) {
-          return;
-        }
-        if (!this._config.alwaysOpen) {
-          const elOpenItem = this._el.querySelector('.accordion__item_show');
-          if (elOpenItem) {
-            elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('accordion__item_show') : null;
-          }
-        }
-        elHeader.parentElement.classList.toggle('accordion__item_show');
-      });
-    }
+  constructor(target, config) {
+    this._el = typeof target === 'string' ? document.querySelector(target) : target;
+    const defaultConfig = {
+      alwaysOpen: true
+    };
+    this._config = Object.assign(defaultConfig, config);
+    this.addEventListener();
   }
 
-  new ItcAccordion('#accordion-1');
-  new ItcAccordion('#accordion-2');
+  addEventListener() {
+    this._el.addEventListener('click', (e) => {
+      const elHeader = e.target.closest('.accordion__header');
+      if (!elHeader) {
+        return;
+      }
+      if (!this._config.alwaysOpen) {
+        const elOpenItem = this._el.querySelector('.accordion__item_show');
+        if (elOpenItem) {
+          elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('accordion__item_show') : null;
+          elOpenItem.querySelector('.accordion__arrow').classList.toggle('arrow-active');
+        }
+      }
+      elHeader.parentElement.classList.toggle('accordion__item_show');
+      elHeader.querySelector('.accordion__arrow').classList.toggle('arrow-active');
+    });
+  }
+}
+
+// Создаем экземпляры аккордеона
+const accordions = document.querySelectorAll('.accordion');
+accordions.forEach((accordion, index) => new ItcAccordion(`#accordion-${index + 1}`));
